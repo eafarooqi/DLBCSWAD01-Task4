c6212b100fecc97574a1123f90d6c2800e122bb3
eafarooqi <e_a_farooqi@hotmail.com>
2023-12-06 16:21:36 +0100

    sort and duplicate items check added

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ function findByBinarySearch(searchArr, target)
         return -1;
     }
 
+    // checking if array is sorted.
+    if(!isArraySorted(searchArr)){
+        console.log("Please provide a sorted array. Binary search works with a sorted array.");
+        return -1;
+    }
+
+    // warning if array has duplicates
+    if(hasArrayDuplicates(searchArr)){
+        console.log("Given array has duplicate values. First found index will be returned.");
+    }
+
     // setting first and last index for the given array
     let startIndex = 0;
     let lastIndex = searchArr.length -1;
@@ -33,6 +44,25 @@ function findByBinarySearch(searchArr, target)
 
     // target doesn't exists in the given search array.
     return -1;
+}
+
+// function to verify if array is sorted (ASC)
+function isArraySorted(arr) 
+{ 
+    for (let i = 0; i < arr.length - 1; i++) { 
+        if (arr[i] > arr[i + 1]) { 
+            return false; 
+        } 
+    } 
+
+    return true; 
+}
+
+// function to check if array has duplicate values.
+function hasArrayDuplicates(arr){
+    return arr.some((item, index) => {
+        return arr.indexOf(item) !== index;
+    });
 }
 
 const sortedList = [1, 5, 10, 11, 16, 18, 25, 50, 51, 60, 70, 99];

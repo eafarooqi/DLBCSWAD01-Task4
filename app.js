e5cee66f75a9a9d493b3df258edb879c2c046fcf
eafarooqi <e_a_farooqi@hotmail.com>
2023-12-06 15:28:37 +0100

    binary sort logic and one test case.

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
 // Function to find an item by binary search in given array
 function findByBinarySearch(searchArr, target)
-{}
+{
+    // if parameters are not provided.
+    if(typeof searchArr === 'undefined' || searchArr.length == 0 || !target){
+        return -1;
+    }
+
+    // setting first and last index for the given array
+    let startIndex = 0;
+    let lastIndex = searchArr.length -1;
+
+    // Looping array until target is found.
+    while (startIndex <= lastIndex) 
+    {
+        // getting the center index of the array by rounding down.
+        let middleIndex = Math.floor((startIndex + lastIndex) / 2)
+
+        // if target is exactly at the middle of the array.
+        // target found! 
+        if (searchArr[middleIndex] === target) {
+            return middleIndex;
+        }
+
+        if (searchArr[middleIndex] < target) {
+          // searching on the right side of the middle index.
+          startIndex = middleIndex + 1
+        } else {
+          // Searching on the left side of the middle index.
+          lastIndex = middleIndex - 1
+        }
+      }
+
+    // target doesn't exists in the given search array.
+    return -1;
+}
+
+const sortedList = [1, 5, 10, 11, 16, 18, 25, 50, 51, 60, 70, 99];
+
+const itemToFind = 25;
+const result = findByBinarySearch(sortedList, itemToFind);
+if (result !== -1) {
+    console.log(`Search Item (${itemToFind}) found at index ${result} in the given array.`);
+} else {
+    console.log(`Search Item (${itemToFind}) not found in the given array.`);
+}
